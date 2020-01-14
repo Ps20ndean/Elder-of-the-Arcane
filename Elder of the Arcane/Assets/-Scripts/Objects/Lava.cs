fefcfe2924b73862fc632dd9bfa381deedcfb7d2
GreatHips <49949549+GreatHips@users.noreply.github.com>
2020-01-14 13:13:33 -0500

    Commenting

--- Elder of the Arcane/Assets/-Scripts/Objects/Lava.cs
+++ Elder of the Arcane/Assets/-Scripts/Objects/Lava.cs
@@ -8,10 +8,19 @@ public class Lava : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // player is equal to the player gameobject
         var player = GameObject.Find("Player");
+
+        // gets the player script attached to the player object
+        var playerComp = player.GetComponent<Player>();
+
         if (collision.gameObject.tag == "Player")
         {
+            // kills the player on touching the lava
             player.GetComponent<HealthManager>().health = 0;
+            
+            // runs the dead method upon dying
+            playerComp.Dead();
         }
     }
 }
